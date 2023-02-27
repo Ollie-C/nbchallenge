acd37f576b59bbe7a2df68f6e6d384494fcc58d8
Ollie Cross <iio.olivercross@gmail.com>
2023-02-27 14:41:54 +0000

    :zap: Updated head and lighthouse report

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,6 +8,12 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Roboto:wght@100&display=swap"
           rel="stylesheet"
         />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta
+          name="description"
+          content="TV bland. Never miss an episode of Eastenders"
+        />
+        <title>TV Bland</title>
       </Head>
       <body>
         <Main />
